70d51f7e1bcede05dae34d496b623a105904a76b
rbprsp <maillikelol@gmail.com>
2025-03-07 16:16:59 +0500

    test changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
 
 #include "parser/parser.h"
 #include "networker/networker.h"
+#include "utils/utils.h"
 
 int main()
 {
     spdlog::set_pattern("[%H:%M:%S.%e] [%^%8l%$] %v");
     spdlog::set_level  (spdlog::level::debug);
 
+    // if(Utils::YesOrNo("Do you want to fetch anilib.me library?"))
+    // {
+    //     Networker n;
+    //     std::string s = "anime.json";
+    //     Parser::SaveFile(s, n.FetchAnimeLib());
+    // }
+    std::string file1 = "anime.json";
+    std::string file2 = "Relony_animes.json";
 
-    spdlog::info("Please choose .json file from shikimori");
-    spdlog::info("Press any key to open file dialog");
-    
-    std::cin.get();
+    auto j1 = Parser::ReadFile(file1);
+    auto j2 = Parser::ReadFile(file2);
 
-    Parser* p = new Parser;
-    p->OpenFile();
+    auto merge = Parser::MergeLists(j1, j2);
 
-
-
+    std::string file3 = "anime_merged.json";
+    Parser::SaveFile(file3, merge);
     return 0;
 }
