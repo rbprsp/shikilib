91086dd8c9a1d1ae3bfcb3b2d3319aa63fe3d51e
rbprsp <maillikelol@gmail.com>
2025-03-12 16:53:48 +0500

    no more static

--- src/main.cpp
+++ src/main.cpp
@@ -16,36 +16,11 @@ int main()
     system("chcp 65001 >nul");
 #endif
 
-    Networker n;
-    if(Utils::YesOrNo("Use authentificated requests?"))
-    {
-        std::cout << "Authorization: ";
-        std::string token = ""; //fix here
-        std::getline(std::cin, token);
-        n.SetToken(token);
-    }
+    Parser p;
+    json anilib = p.ReadFile("anilib.json");
+    json shiki  = p.ReadFile("shiki.json");
+    json merged = p.Merge(anilib["data"], shiki);
+    p.SaveFile("shikilib.json", merged);
 
-    if(Utils::YesOrNo("Do you want to fetch anilib.com?"))
-        Parser::SaveFile("anilib.json", n.FetchAnimeLib());
-
-    nlohmann::json anilib = Parser::ReadFile("anilib.json");
-    nlohmann::json shiki  = Parser::ReadFile("shiki.json");
-    nlohmann::json merged = Parser::MergeLists(anilib, shiki);
-    nlohmann::json not_found {};
-    if(std::filesystem::exists("not_found.json"))
-        not_found = Parser::ReadFile("not_found.json");
-
-    nlohmann::json merged_not_found = Parser::MergeNotFound(anilib, not_found);
-
-    merged.insert(merged.end(), merged_not_found.begin(), merged_not_found.end());
-
-    Parser::SaveFile("shikilib.json", merged);
-
-    spdlog::critical("If there is still some not found anime, please add them manualy, I'll fix them later");
-    if(Utils::YesOrNo("Would you like to update your anilib.me profile?"))
-    {
-        n.AddToAnimeLibFromJson(merged);
-        return 0;
-    }
     return 0;
 }
